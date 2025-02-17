e739540c0c8093f268b03b0354686393eb60cdf0
Dalton <daltonfiggdavies@gmail.com>
2025-02-17 15:35:57 +0000

    Set up basic events

---
+++ events/interactionCreate.js
@@ -0,0 +1,28 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.InteractionCreate,
+
+  async execute(interaction) {
+    // Every time a slash command is used, it is an ineraction
+    if (interaction.isChatInputCommand()) {
+      // Access the command which has been run
+      const command = interaction.client.commands.get(interaction.commandName);
+
+      // Ignore if there are no matching commands
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      // Execute the command
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+};
