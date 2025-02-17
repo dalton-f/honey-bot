b76a801493711af61c4a69c46ba69b88560327e0
Dalton <daltonfiggdavies@gmail.com>
2025-02-17 15:36:13 +0000

    Add basic command registering and bot setup

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const {
+  REST,
+  Routes,
+  Client,
+  Collection,
+  GatewayIntentBits,
+} = require("discord.js");
 
-// Create a new client instance
+const { clientId, guildId, token } = require("./config.json");
+
+const fs = require("node:fs");
+const path = require("node:path");
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once).
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const commands = [];
+
+client.commands = new Collection();
+
+// Grab all the command folders from the commands directory
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  // Grab all the command files from the commands directory
+  const commandsPath = path.join(foldersPath, folder);
+
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+      client.commands.set(command.data.name, command);
+    }
+  }
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// Deploy the commands
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application  commands.`);
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log(`Successfully reloaded ${data.length} application commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const eventsPath = path.join(__dirname, "events");
+
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 // Log in to Discord with your client's token
 client.login(token);
