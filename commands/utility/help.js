2f49a4750242e7ac2b4031d9c38a5c00609367f8
Dalton <daltonfiggdavies@gmail.com>
2025-02-19 07:53:36 +0000

    Refactor help command to be an embed

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,11 +1,26 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("Returns a list of the bot commands"),
+    .setDescription("Returns this list of the bot commands"),
 
   async execute(interaction) {
-    await interaction.reply("TODO: List of commands");
+    const commands = interaction.client.commands;
+    let commandList = "";
+
+    commands.forEach((command) => {
+      commandList += `**/${command.data.name}**: ${command.data.description}\n`;
+    });
+
+    const embed = new EmbedBuilder()
+      .setTitle("Honey Bot - Info")
+      .setDescription(
+        `Honey Bot is a multipurpose discord bot made by @Dalton. This command shows information about all the commands you can run using this bot.\n\n**Commands:**\n\n${commandList}`
+      )
+      .setColor("#ce98ec")
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
